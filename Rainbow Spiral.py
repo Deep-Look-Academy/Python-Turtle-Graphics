86360311c7b520c8db033868307ce1f7b19b3e7f
deep_look_academy <iqraacademynm@gmail.com>
2022-12-03 23:19:05 +0600

    rainbow spiral

---
+++ Rainbow Spiral.py
@@ -0,0 +1,14 @@
+from turtle import*
+pensize(2)
+hideturtle()
+speed('fastest')
+bgcolor('black')
+colors = colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow'] 
+
+
+for l in range(250):
+    color(colors[l%len(colors)])
+    forward(l)
+    left(61)
+
+exitonclick()
